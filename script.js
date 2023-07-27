d577487c3c17eb5699b1fc3df0eaddb654b24ac5
PratheekTheCoder <pratheekTheCoder@gmail.com>
2023-07-27 15:31:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function showTime(){
+    const date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let session = "AM";
+
+        if (hours == 0) {
+            hours = 12;
+        }
+
+        if (hours > 12) {
+            hours = hours - 12;
+            session = "PM";
+            
+        }
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes  < 10) ? "0" + minutes : minutes;
+        seconds = (seconds  < 10) ? "0" + seconds : seconds;
+
+        let time = hours + ":" + minutes + ":" + seconds + ":" + session;
+        document.getElementById("display").innerText = time;
+        document.getElementById("display").textContent = time;
+        
+
+        setTimeout(showTime, 1000);
+}
+showTime();
